fdadbf4701098e2439812404922646025e463a95
Brahms <yangkaiyuan@u.nus.edu>
2018-04-03 12:47:41 +0800

    ignore package-lock, add Homepage and Loginpage route

---
+++ README.md
@@ -0,0 +1,1 @@
+App Component is going to be like a route dispatcher. It is going to render depending on the route. 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
-const App = () => <div>Hello World</div>;
+const App = () => (
+  <div>
+    <Route exact path='/' component={HomePage} />
+    <Route path='/login' component={LoginPage} />
+  </div>
+);
 
 export default App;
